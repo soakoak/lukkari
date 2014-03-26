ae934d801b3f63b33283d9c18776567acfd03c46
moonspliced <jedatermy87@gmail.com>
2014-03-26 14:38:05 +0200

    Copied old schema as base

---
+++ MetropoliaLukkari/src/fi/sokira/lukkari/provider/DbSchema.java
@@ -0,0 +1,32 @@
+package fi.sokira.lukkari.provider;
+
+import android.provider.BaseColumns;
+
+
+interface DbSchema {
+	
+	final static String DATABASE_NAME = "lukkari.db";
+
+//	final static String TBL_CURRENCIES = "currency";
+//
+//	final static String COL_ID = BaseColumns._ID;
+//	final static String COL_NAME = "name";
+//	final static String COL_RATE = "rate";
+//	final static String COL_DATE = "date";
+//
+//	final static String[] FIELDS = { COL_ID, COL_NAME, COL_RATE,
+//			COL_DATE };
+//
+//	final static String CREATE_TABLE_CURRENCIES = "CREATE TABLE " + TBL_CURRENCIES + 
+//			"(" + 
+//					COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + 
+//					COL_NAME + " TEXT, " + 
+//					COL_RATE + " TEXT, " + 
+//					COL_DATE + " TEXT" + 
+//			")";
+//	
+//	final static String DROP_TBL_CURRENCIES = "DROP TABLE IF EXISTS " + TBL_CURRENCIES;
+//
+//	final static String DEFAULT_TBL_CURRENCIES_SORT_ORDER = COL_NAME
+//			+ " ASC";
+}
