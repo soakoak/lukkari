1cb413d461dbbe19cfffd89933e508bdd7f802d4
soakoak <jedatermy87@gmail.com>
2014-04-18 15:50:21 +0300

    Tietokannasta osia uudelleennimetty

--- MetropoliaLukkari/src/fi/sokira/lukkari/provider/DbSchema.java
+++ MetropoliaLukkari/src/fi/sokira/lukkari/provider/DbSchema.java
@@ -13,12 +13,13 @@ public interface DbSchema {
 	final static String TBL_VARAUS = "varaus";
 
 	final static String COL_ID = BaseColumns._ID;
-	final static String COL_NIMI = "nimi";
-	final static String COL_TUNNUS = "tunnus";
-	final static String COL_RYHMATUNNUS = "ryhmatunnus";
+	final static String COL_NAME = "name"; //Nimi
+	final static String COL_CODE = "code"; //Tunnus
+	//TODO muuta studentGroups omaksi tableksi
+	final static String COL_STUDENT_GROUPS = "studentGroups";
 	final static String COL_TILA = "tila";
-	final static String COL_ALOITUS_PVM = "aloitus_pvm";
-	final static String COL_LOPPU_PVM = "loppu_pvm";
+	final static String COL_START_DATE = "startDate";
+	final static String COL_END_DATE = "endDate";
 	final static String COL_ID_LUKKARI = "id_lukkari";
 	final static String COL_ID_TOTEUTUS = "id_toteutus";
 	
@@ -26,17 +27,17 @@ public interface DbSchema {
 	final static String CREATE_TABLE_LUKKARI = "CREATE TABLE " + TBL_LUKKARI + 
 			"(" + 
 					COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + 
-					COL_NIMI + " TEXT" + 
+					COL_NAME + " TEXT" + 
 			")";
 	
 	final static String CREATE_TABLE_TOTEUTUS = "CREATE TABLE " + TBL_TOTEUTUS + 
 			"(" + 
 					COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + 
-					COL_TUNNUS + " TEXT, " + 
-					COL_NIMI + " TEXT, " +
-					COL_RYHMATUNNUS + " TEXT, " +
-					COL_ALOITUS_PVM + " INTEGER, " +
-					COL_LOPPU_PVM + " INTEGER" +
+					COL_CODE + " TEXT, " + 
+					COL_NAME + " TEXT, " +
+					COL_STUDENT_GROUPS + " TEXT, " +
+					COL_START_DATE + " INTEGER, " +
+					COL_END_DATE + " INTEGER" +
 			")";
 	
 	final static String CREATE_TABLE_LUKKARI_TO_TOTEUTUS = "CREATE TABLE " + TBL_LUKKARI_TO_TOTEUTUS + 
@@ -51,8 +52,8 @@ public interface DbSchema {
 					COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + 
 					COL_ID_TOTEUTUS + " INTEGER NOT NULL REFERENCES " + TBL_TOTEUTUS + "(" + COL_ID + ") ON DELETE CASCADE ON UPDATE CASCADE, " +
 					COL_TILA + " TEXT, " +
-					COL_ALOITUS_PVM + " INTEGER, " +
-					COL_LOPPU_PVM + " INTEGER" +
+					COL_START_DATE + " INTEGER, " +
+					COL_END_DATE + " INTEGER" +
 			")";
 	
 	final static String DROP_TBL_LUKKARI = "DROP TABLE IF EXISTS " + TBL_LUKKARI;
--- MetropoliaLukkari/src/fi/sokira/lukkari/provider/LukkariContract.java
+++ MetropoliaLukkari/src/fi/sokira/lukkari/provider/LukkariContract.java
@@ -9,7 +9,7 @@ public class LukkariContract {
 	public static final String AUTHORITY = "fi.sokira.lukkari.provider";
 	public static final Uri CONTENT_URI = Uri.parse( "content://" + AUTHORITY);
 	
-	public static final class Toteutus implements BaseColumns {
+	public static final class Realization implements BaseColumns {
 		
 		public static final Uri CONTENT_URI = Uri.withAppendedPath(
 				LukkariContract.CONTENT_URI, DbSchema.TBL_TOTEUTUS);
@@ -24,20 +24,20 @@ public class LukkariContract {
 				ContentResolver.CURSOR_ITEM_BASE_TYPE
 				+ MIME_END;
 		
-		public static final String TUNNUS = DbSchema.COL_TUNNUS;
+		public static final String CODE = DbSchema.COL_CODE;
 		
-		public static final String NIMI = DbSchema.COL_NIMI;
+		public static final String NAME = DbSchema.COL_NAME;
 		
-		public static final String RYHMATUNNUS = DbSchema.COL_RYHMATUNNUS;
+		public static final String STUDENT_GROUPS = DbSchema.COL_STUDENT_GROUPS;
 		
-		public static final String ALOITUS_PVM = DbSchema.COL_ALOITUS_PVM;
+		public static final String START_DATE = DbSchema.COL_START_DATE;
 		
-		public static final String LOPPU_PVM = DbSchema.COL_LOPPU_PVM;
+		public static final String END_DATE = DbSchema.COL_END_DATE;
 		
 		public static final String[] PROJECTION_ALL = {
-				_ID, TUNNUS, NIMI, RYHMATUNNUS, ALOITUS_PVM, LOPPU_PVM
+				_ID, CODE, NAME, STUDENT_GROUPS, START_DATE, END_DATE
 		};
 		
-		public static final String SORT_ORDER_DEFAULT = NIMI + "ASC";
+		public static final String SORT_ORDER_DEFAULT = NAME + "ASC";
 	}
 }
