b6bdb8c2a94dee2cb4a9be962d95a296cb265422
soakoak <jedatermy87@gmail.com>
2014-04-18 12:10:15 +0300

    Toimiva yhteys (Apache HttpClient)

--- MetropoliaLukkari/src/fi/sokira/metropolialukkari/HakuFragment.java
+++ MetropoliaLukkari/src/fi/sokira/metropolialukkari/HakuFragment.java
@@ -1,19 +1,22 @@
 package fi.sokira.metropolialukkari;
 
-import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.UnsupportedEncodingException;
 
-import javax.net.ssl.HttpsURLConnection;
-
+import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import android.app.Activity;
 import android.app.Fragment;
@@ -76,7 +79,17 @@ public class HakuFragment extends Fragment implements OnClickListener {
 			NetworkInfo netInfo = connMgr.getActiveNetworkInfo();
 			
 			if( netInfo != null && netInfo.isConnected()) {
-				new LukkariWebLoadTask().execute( "https://opendata.metropolia.fi/r1/reservation/search");
+//				new LukkariWebLoadTask().execute( "https://opendata.metropolia.fi/r1/reservation/search");
+				Bundle args = new Bundle(16);
+				args.putStringArray( 
+						LukkariWebLoadTask.VALUE_STUDENT_GROUPS, 
+						new String[]{ 
+							groupInput.getText().toString()
+						});
+				
+				Log.d(TAG, groupInput.getText().toString());
+			
+				new LukkariWebLoadTask().execute( args);
 				Toast.makeText(getActivity(), "We have data.", Toast.LENGTH_LONG).show();
 			} else {
 				Toast.makeText(getActivity(), "Aaaaaand it's gone.", Toast.LENGTH_LONG).show();
@@ -95,109 +108,108 @@ public class HakuFragment extends Fragment implements OnClickListener {
 		public void onSearchInitiated();
 	}
 	
-	private class LukkariWebLoadTask extends AsyncTask<String, Void, String> {
+	private class LukkariWebLoadTask extends AsyncTask<Bundle, Void, String> {
+		
+		public static final String VALUE_NAME = "name";
+		public static final String VALUE_START_DATE = "startDate";
+		public static final String VALUE_END_DATE = "endDate";
+		public static final String VALUE_STUDENT_GROUPS = "studentGroups";
 
 		private static final String TAG = "LukkariWebLoadTask";
 		
+		private static final String reservationServiceUrl = "https://opendata.metropolia.fi/r1/reservation";
+		private static final String realizationServiceUrl = "https://opendata.metropolia.fi/r1/realization";
+		
+		public LukkariWebLoadTask() {
+		}
+		
 		@Override
-		protected String doInBackground(String... urls) {
+		protected String doInBackground(Bundle... params) {
+
+			String apikey = Secrets.METROPOLIA_API_KEY;
+			String contentType = "application/json";
+			String charset = HTTP.UTF_8;
 			
-			StringBuilder strb = new StringBuilder();
+			DefaultHttpClient client = new DefaultHttpClient();
 			
-			URL url = null;
-			HttpsURLConnection conn = null;
-			int statusCode = 0;
+			HttpParams clientParams = client.getParams();
+			HttpConnectionParams.setConnectionTimeout( clientParams, 30000);
+			HttpConnectionParams.setSoTimeout(clientParams, 30000);			
+			
+			HttpPost postMethod = new HttpPost( realizationServiceUrl + "/search?apiKey=" + apikey);
+			
+			Bundle values = params[0];
+			JSONObject query = new JSONObject();
 			
 			try {
-				url = new URL(urls[0]);
-			} catch ( MalformedURLException me) {
-			     Log.e(TAG, "URL could not be parsed. URL : " + urls[0] + ".");
-			     me.printStackTrace();
+				if( values.containsKey( VALUE_NAME)) {
+					query.put( VALUE_NAME, values.getString( VALUE_NAME));
+				}
+				
+				if( values.containsKey( VALUE_START_DATE)) {
+					query.put(VALUE_START_DATE, values.getString( VALUE_START_DATE));
+				}
+				
+				if( values.containsKey( VALUE_END_DATE)) {
+					query.put( VALUE_END_DATE, values.getString( VALUE_END_DATE));
+				}
+				
+				if( values.containsKey( VALUE_STUDENT_GROUPS)) {
+					Log.d(TAG, "We have student groups");
+					query.put( VALUE_STUDENT_GROUPS, asJsonArray( values.getStringArray( VALUE_STUDENT_GROUPS)));
+				}
+				
+				Log.d(TAG, "Written JSON: " + query.toString());
+			} catch (JSONException e) {
+				Log.e(TAG, "Error while writing JSON");
+				e.printStackTrace();
 			}
 			
 			try {
-				conn = (HttpsURLConnection) url.openConnection();
-				conn.setReadTimeout( 10000);
-				conn.setConnectTimeout( 15000);
-				conn.setRequestMethod( "POST");
-				
-				String base64auth = Secrets.METROPOLIA_API_KEY;
-				conn.setRequestProperty("Authorization", "Basic " + base64auth);
-				conn.setRequestProperty("Content-Type", "application/json");
-				conn.setRequestProperty("Accept", "application/json");
-
-				conn.setDoInput( true);
-				conn.setDoOutput( true);
-				
-				JSONObject query = null;
-				
+				StringEntity entity = new StringEntity( query.toString());
+				entity.setContentType(contentType);
+				entity.setContentEncoding( charset);
+				postMethod.setEntity( entity);
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+			
+			HttpResponse response = null;
+			try {
+				response = client.execute( postMethod);
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} 
+			
+			if( response != null) {
 				try {
-					query = new JSONObject();
 					
-					query.put("startDate", "2013-11-22T09:00");
-					
-					JSONArray room = new JSONArray();
-					room.put("U203");
-					
-					query.put("room", room);
-					
-					Log.d(TAG, "Written JSON: " + query.toString());
-				} catch (JSONException e) {
-					Log.e(TAG, "Error while writing JSON");
+					return EntityUtils.toString( response.getEntity(), charset);
+				} catch (ParseException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
 					e.printStackTrace();
 				}
-				
-				OutputStreamWriter wr = new OutputStreamWriter( conn.getOutputStream());
-				wr.write( query.toString());
-				wr.flush();
-				
-				try {
-					statusCode = conn.getResponseCode();
-				} catch (IOException e) {
-					statusCode = conn.getResponseCode();
-					Log.d( TAG, "The response is: " + statusCode);
-				}
-				
-//				conn.connect();
-				
-				InputStream in = new BufferedInputStream( conn.getInputStream());
-				
-				try {
-					statusCode = conn.getResponseCode();
-				} catch (IOException e) {
-					statusCode = conn.getResponseCode();
-					Log.d( TAG, "The response is: " + statusCode);
-				}
-				
-				Reader reader = null;
-				reader = new InputStreamReader( in, "UTF-8");
-				
-				int len = 512;
-				char[] buffer = new char[len];
-				int numChars;
-				numChars = reader.read( buffer);
-				
-				while( numChars != -1) {
-					strb.append( buffer, 0, numChars);
-					numChars = reader.read( buffer);
-					Log.d( TAG, "Builder length: " + strb.length());
-				}
-				
-				Log.d(TAG, "Reading ended.");
-			
-			} catch( IOException e) {
-				Log.e(TAG, "Unable to retrieve the web page. URL may be invalid.");
-				e.printStackTrace();
-			} finally {
-				conn.disconnect();
 			}
 			
-			return strb.toString();
+			return "noResult";
 		}
 		
 		@Override
 		protected void onPostExecute(String result) {
 			Log.d(TAG, "Result: " + result);
+		}
+		
+		JSONArray asJsonArray( String[] strings) {
+			JSONArray vals = new JSONArray();
+			
+			for (String str : strings) {
+				vals.put( str);
+			}
+			
+			return vals;
 		}
 	}
 }
