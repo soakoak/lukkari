5bb3a0e0ee18aa0071d70ce13529c88a0ecdff33
soakoak <jedatermy87@gmail.com>
2014-04-28 19:21:45 +0300

    Tietokannan parantelua. Lukkaritietojen UI:sta lisäys toimii.

--- MetropoliaLukkari/src/fi/sokira/metropolialukkari/LukkariActivity.java
+++ MetropoliaLukkari/src/fi/sokira/metropolialukkari/LukkariActivity.java
@@ -1,6 +1,7 @@
 package fi.sokira.metropolialukkari;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 import android.app.Activity;
 import android.app.Fragment;
@@ -15,6 +16,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 import fi.sokira.lukkari.provider.DatabaseHelper;
 import fi.sokira.lukkari.provider.DbSchema;
 import fi.sokira.lukkari.provider.LukkariContract.Lukkari;
@@ -23,8 +25,10 @@ import fi.sokira.metropolialukkari.models.Realization;
 import fi.sokira.metropolialukkari.models.RealizationResult;
 import fi.sokira.metropolialukkari.models.Reservation;
 import fi.sokira.metropolialukkari.models.ReservationResult;
+import fi.sokira.metropolialukkari.models.Resource;
 import fi.sokira.metropolialukkari.models.Result;
 import fi.sokira.metropolialukkari.models.ResultItem;
+import fi.sokira.metropolialukkari.models.StudentGroup;
 
 public class LukkariActivity extends Activity
 		implements
@@ -212,39 +216,22 @@ public class LukkariActivity extends Activity
 	
 	@Override
 	public void onResultItemsAdded(ArrayList<ResultItem> items, int itemType) {
+			
+		switch( itemType) {
 		
-//		SQLiteOpenHelper helper = new DatabaseHelper(getApplication());
-//		SQLiteDatabase db = helper.getWritableDatabase();
-//		
-//		db.beginTransaction();
-//		
-//		try {
-//			ContentValues values = new ContentValues();
+		case ToteutusListFragment.TYPE_REALIZATION :
+			new SqlRealizationAddingTask().execute( 
+					items.toArray( new Realization[ items.size()]));
+			break;
 			
-			switch( itemType) {
+		case ToteutusListFragment.TYPE_RESERVATION :
+			new SqlReservationAddingTask().execute( 
+					items.toArray( new Reservation[ items.size()]));
+			break;
 			
-			case ToteutusListFragment.TYPE_REALIZATION :
-				new SqlRealizationAddingTask().execute( 
-						items.toArray( new Realization[ items.size()]));
-				
-				break;
-				
-			case ToteutusListFragment.TYPE_RESERVATION :
-				new SqlReservationAddingTask().execute( 
-						items.toArray( new Reservation[ items.size()]));
-				
-				break;
-				
-			default:
-				break;
-			}
-			
-//			db.setTransactionSuccessful();
-//		} catch( SQLException e) {
-//			Log.d(TAG, "Error while inserting to db");
-//		} finally {
-//			db.endTransaction();
-//		}
+		default:
+			break;
+		}
 	}
 
 	private class SqlRealizationAddingTask extends AsyncTask<Realization, Void, Boolean> {
@@ -253,29 +240,88 @@ public class LukkariActivity extends Activity
 		
 		@Override
 		protected Boolean doInBackground(Realization... params) {
-//			Log.d(TAG, "Needs implementation!");
-			
 			SQLiteOpenHelper helper = new DatabaseHelper(getApplication());
 			SQLiteDatabase db = helper.getWritableDatabase();
-			SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
 			
-			builder.setTables( DbSchema.TBL_LUKKARI);
-			builder.appendWhere( 
-					DbSchema.COL_NAME + " = '" + TEST_LUKKARI_NAME + "'");
-			Cursor cursor = builder.query(db, 
-					new String[]{ Lukkari._ID,  Lukkari.LUKKARI_NAME}, 
-					null,
-					null, 
-					null, 
-					null,
-					Lukkari.LUKKARI_NAME + " ASC");
+			Cursor cursor = getLukkariByName(db, TEST_LUKKARI_NAME);
 			cursor.moveToFirst();
-			int idx = cursor.getInt( cursor.getColumnIndex( DbSchema.COL_ID));
+			int lukId = cursor.getInt( cursor.getColumnIndex( DbSchema.COL_ID));
 			
 			Log.d(TAG, "Tulosten m‰‰r‰: " + cursor.getCount());
-			Log.d(TAG, "Lukkarin " + TEST_LUKKARI_NAME + " indeksi: " + idx);
+			Log.d(TAG, "Lukkarin " + TEST_LUKKARI_NAME + " indeksi: " + lukId);
 			
-			return false;
+			ContentValues values = new ContentValues();
+			long relzId, groupId;
+			
+			db.beginTransaction();
+			try {
+				for( Realization relz : params) {
+					values.clear();
+					values.put( DbSchema.COL_CODE, relz.getCode());
+					values.put( DbSchema.COL_NAME, relz.getName());
+					values.put( DbSchema.COL_START_DATE, relz.getStartDate().getTime());
+					values.put( DbSchema.COL_END_DATE, relz.getEndDate().getTime());
+					relzId = db.insertWithOnConflict( DbSchema.TBL_REALIZATION, 
+							null, values, SQLiteDatabase.CONFLICT_REPLACE);
+					
+					for( StudentGroup group : relz.getStudentGroups()) {
+						values.clear();
+						values.put( DbSchema.COL_CODE, group.getCode());
+						groupId = db.insertWithOnConflict( DbSchema.TBL_STUDENT_GROUP, 
+								null, values, SQLiteDatabase.CONFLICT_REPLACE);
+						
+						values.clear();
+						values.put( DbSchema.COL_ID_REALIZATION, relzId);
+						values.put( DbSchema.COL_ID_STUDENT_GROUP, groupId);
+						db.insertWithOnConflict( 
+								DbSchema.TBL_REALIZATION_TO_STUDENT_GROUP, 
+								null, values, SQLiteDatabase.CONFLICT_IGNORE);
+					}
+					
+					values.clear();
+					values.put( DbSchema.COL_ID_LUKKARI, lukId);
+					values.put( DbSchema.COL_ID_REALIZATION, relzId);
+					db.insertWithOnConflict( 
+							DbSchema.TBL_LUKKARI_TO_REALIZATION, 
+							null, values, SQLiteDatabase.CONFLICT_IGNORE);
+				}
+				
+				db.setTransactionSuccessful();			
+			} catch (SQLException e) {
+				Log.d(TAG, "Error while inserting to db");
+				return false;
+			} finally {
+				db.endTransaction();
+			}
+			
+			SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
+			
+			builder.setTables( DbSchema.TBL_REALIZATION);
+			cursor = builder.query(db, 
+					new String[]{ DbSchema.COL_ID, DbSchema.COL_CODE}, 
+					null,
+					null, 
+					null, 
+					null,
+					DbSchema.COL_CODE + " ASC");
+			cursor.moveToFirst();
+
+			Log.d(TAG, "Toteutusten m‰‰r‰: " + cursor.getCount());
+			
+			return true;
+		}
+		
+		@Override
+		protected void onPostExecute(Boolean result) {
+			if( result) {
+				Toast.makeText( getApplication(), 
+						"Toteutukset lis‰tty onnistuneeti.", 
+						Toast.LENGTH_LONG).show();
+			} else {
+				Toast.makeText( getApplication(), 
+						"Virhe lis‰tess‰ toteutuksia.", 
+						Toast.LENGTH_LONG).show();
+			}
 		}
 	}
 	
@@ -285,8 +331,147 @@ public class LukkariActivity extends Activity
 		
 		@Override
 		protected Boolean doInBackground(Reservation... params) {
-			Log.d(TAG, "Needs implementation!");
-			return false;
+			SQLiteOpenHelper helper = new DatabaseHelper(getApplication());
+			SQLiteDatabase db = helper.getWritableDatabase();
+			SQLiteQueryBuilder builder = null;
+			ContentValues values = new ContentValues();
+			
+			for( Reservation reservation : params) {	
+				Resource realization = 
+						findResource( reservation.getResources(), Resource.TYPE_REALIZATION);
+				if( realization.getCode().isEmpty()) {
+					Log.d(TAG, "Tyhj‰ koodi");
+					continue;
+				}
+				
+				builder = new SQLiteQueryBuilder();
+				builder.setTables( DbSchema.TBL_REALIZATION);
+				builder.appendWhere(
+						DbSchema.COL_CODE + " = '" + 
+						realization.getCode() + "'");
+				Cursor cursor = builder.query(db, 
+						new String[]{ DbSchema.COL_ID, DbSchema.COL_CODE }, 
+						null,
+						null, 
+						null, 
+						null,
+						DbSchema.COL_CODE + " ASC");
+				
+				long relzId;
+				
+				if( cursor.getCount() > 0) {
+					cursor.moveToFirst();
+					relzId = cursor.getInt( cursor.getColumnIndex( DbSchema.COL_ID));
+				} else {
+					db.beginTransaction();
+					try {
+						values.clear();
+						values.put( DbSchema.COL_CODE, realization.getCode());
+						values.put( DbSchema.COL_NAME, realization.getName());
+						relzId = db.insertWithOnConflict( DbSchema.TBL_REALIZATION, 
+								null, values, SQLiteDatabase.CONFLICT_IGNORE);
+						
+						int lukId = 
+							getId( getLukkariByName(db, TEST_LUKKARI_NAME), 0);
+						values.clear();
+						values.put( DbSchema.COL_ID_LUKKARI, lukId);
+						values.put( DbSchema.COL_ID_REALIZATION, relzId);
+						db.insertWithOnConflict( 
+								DbSchema.TBL_LUKKARI_TO_REALIZATION, 
+								null, values, SQLiteDatabase.CONFLICT_IGNORE);
+						
+						db.setTransactionSuccessful();
+					} catch (SQLException e) {
+						Log.d(TAG, "Error while inserting to db");
+						return false;
+					} finally {
+						db.endTransaction();
+					}
+				}
+				
+				db.beginTransaction();
+				try {
+					String roomCode = findResource( 
+							reservation.getResources(), 
+							Resource.TYPE_ROOM)
+						.getCode();
+					values.clear();
+					values.put( DbSchema.COL_ID_REALIZATION, relzId);
+					values.put( DbSchema.COL_ROOM, roomCode);
+					values.put( DbSchema.COL_START_DATE, 
+							reservation.getStartDate().getTime());
+					values.put( DbSchema.COL_END_DATE, 
+							reservation.getEndDate().getTime());
+					long resId = db.insertWithOnConflict( 
+							DbSchema.TBL_RESERVATION, 
+							null, values, SQLiteDatabase.CONFLICT_REPLACE);
+					Log.d(TAG, "Lis‰tyn varauksen rivi tietokannassa: " + resId);
+					
+					db.setTransactionSuccessful();
+				} catch (SQLException e) {
+					Log.d(TAG, "Error while inserting to db");
+					return false;
+				} finally {
+					db.endTransaction();
+				}
+			}
+			
+			builder = new SQLiteQueryBuilder();
+			builder.setTables( DbSchema.TBL_RESERVATION);
+			Cursor cursor = builder.query(db, 
+					new String[]{ DbSchema.COL_ID }, 
+					null,
+					null, 
+					null, 
+					null,
+					null);
+			cursor.moveToFirst();
+
+			Log.d(TAG, "Varausten m‰‰r‰: " + cursor.getCount());
+			
+			return true;
 		}
+		
+		@Override
+		protected void onPostExecute(Boolean result) {
+			if( result) {
+				Toast.makeText( getApplication(), 
+						"Varaukset lis‰tty onnistuneeti.", 
+						Toast.LENGTH_LONG).show();
+			} else {
+				Toast.makeText( getApplication(), 
+						"Virhe lis‰tess‰ varauksia.", 
+						Toast.LENGTH_LONG).show();
+			}
+		}
+		
+		private Resource findResource( Collection<Resource> resources, String resourceType) {
+			for( Resource resource : resources) {
+				if( resource.getType().equals( resourceType)) {
+					return resource;
+				}
+			}
+			return null;
+		}
+	}
+	
+	protected Cursor getLukkariByName( SQLiteDatabase db, String lukkariName) {
+		SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
+		
+		builder.setTables( DbSchema.TBL_LUKKARI);
+		builder.appendWhere( 
+				DbSchema.COL_NAME + " = '" + lukkariName + "'");
+		return builder.query(db, 
+				new String[]{ Lukkari._ID,  Lukkari.LUKKARI_NAME}, 
+				null,
+				null, 
+				null, 
+				null,
+				Lukkari.LUKKARI_NAME + " ASC");
+	}
+	
+	protected int getId( Cursor cursor, int idx) {
+		cursor.moveToPosition( idx);
+		return cursor.getInt( cursor.getColumnIndex( DbSchema.COL_ID));
 	}
 }
