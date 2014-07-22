041a83b023f769046517aa235a5019324bf35ed2
soakoak <jedatermy87@gmail.com>
2014-07-22 16:43:33 +0300

    Unique insert-function removed from LukkariDao.

--- MetropoliaLukkari/src/fi/sokira/lukkari/provider/LukkariDao.scala
+++ MetropoliaLukkari/src/fi/sokira/lukkari/provider/LukkariDao.scala
@@ -16,26 +16,4 @@ object LukkariDao extends AbstractDao {
    override def uniqueSelection(values: ContentValues): (String, Array[String]) = {
       (Columns.NAME + " = ?", Array(values.getAsString(Columns.NAME)))
    }
-   
-   override def insert(writeableDb: SQLiteDatabase, values: ContentValues): Long = {
-      try {
-         writeableDb.insertOrThrow(tableName, null, values)
-      } catch {
-         case ex: SQLException =>
-            Log.d(this.Tag, "There was an existing record.")
-            val (selection, selectionArgs) = uniqueSelection(values)
-            val cursor = query(writeableDb, 
-               Array(columnId),
-               selection,
-               selectionArgs,
-               null)
-            Log.d(Tag, "Cursor size: " + cursor.getCount)
-            val id = extractFirstId(cursor)
-
-            val updateSelection = columnId + " = " + id
-            update(writeableDb, values, updateSelection, Array())
-            
-            id
-      }
-   }
 }
